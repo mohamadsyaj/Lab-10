c2e879983eedd143b5e52389996715a796445176
Mohamad syaj <mbsyaj@umich.edu>
2024-04-11 23:29:28 -0400

    changed numbers in the branch

--- src/lab10/testSelectionSort.java
+++ src/lab10/testSelectionSort.java
@@ -73,12 +73,12 @@ public class testSelectionSort {
     	int[] arr = new int[5];
         arr[0] = -5;
         arr[1] = -3;
-        arr[2] = -8;
+        arr[2] = -10;
         arr[3] = 10;
         arr[4] = 2;
         
         int[] Sortedarr = new int[5];
-        Sortedarr[0] = -8;
+        Sortedarr[0] = -10;
         Sortedarr[1] = -5;
         Sortedarr[2] = -3;
         Sortedarr[3] = 2;
@@ -94,7 +94,7 @@ public class testSelectionSort {
     public void testDuplicates(){
     	int[] arr = new int[5];
         arr[0] = 8;
-        arr[1] = 8;
+        arr[1] = 9;
         arr[2] = 9;
         arr[3] = 10;
         arr[4] = 2;
@@ -102,7 +102,7 @@ public class testSelectionSort {
         int[] Sortedarr = new int[5];
         Sortedarr[0] = 2;
         Sortedarr[1] = 8;
-        Sortedarr[2] = 8;
+        Sortedarr[2] = 9;
         Sortedarr[3] = 9;
         Sortedarr[4] = 10;
         
